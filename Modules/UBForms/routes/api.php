b614c55215ab946466f8c6f1494962dbf90861e5
root <root@d4bed9d2d86d.ub.edu.bz>
2025-05-08 16:33:11 -0600

    asdfasdf

--- Modules/UBForms/routes/api.php
+++ Modules/UBForms/routes/api.php
@@ -14,6 +14,108 @@ use Modules\UBForms\Http\Controllers\UBFormsController;
  *
 */
 
-Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
-    Route::apiResource('ubforms', UBFormsController::class)->names('ubforms');
+//This will be the only unprotected route because this is used for authentication
+Route::post('authenticate', [AuthController::class, 'AuthenticateUser']);
+
+
+Route::group(['middleware' => ['auth:sanctum']], function(){
+    // Add any route that needs to be protected inside this group
+
+    //Initialize
+    Route::post('/facultyInitialize', [FacultyController::class, 'initialize']); //This route should get the data that is passed in the UI 
+
+    Route::post('/staffInitialize', [StaffController::class, 'initialize']); //This route should get the data that is passed in the UI 
+
+    Route::post('/recordsInitialize', [RecordsStatistics::class, 'initialize']); //This route should get the data that is passed in the UI 
+
+    Route::post('/HRInitialize', [HRStatistics::class, 'initialize']); //This route should get the data that is passed in the UI 
+
+    //Fixed this, was calling the wrong controller
+    Route::post('/financeInitialize', [FinanceStatistics::class, 'initialize']); //This route should get the data that is passed in the UI 
+
+    //Create
+
+    Route::post('/facultyReport', [FacultyController::class, 'store']); //This route should get the data that is passed in the UI 
+
+    Route::post('/staffReport', [StaffController::class, 'store']); //This route should get the data that is passed in the UI 
+
+    Route::post('/recordsReport', [RecordsStatistics::class, 'store']); //This route should get the data that is passed in the UI 
+
+    Route::post('/HRReport', [HRStatistics::class, 'store']); //This route should get the data that is passed in the UI 
+
+    Route::post('/financeReport', [FinanceStatistics::class, 'store']); //This route should get the data that is passed in the UI 
+
+    //Read 
+
+    Route::get('/facultyReport/{reportID}', [FacultyController::class, 'getReport']); //This route should get the data that is passed in the UI 
+    Route::get('/facultyReportByUser', [FacultyController::class, 'getReportByUser']); //This route should get the data that is passed in the UI 
+
+    Route::get('/staffReport/{reportID}', [StaffController::class, 'getReport']); //This route should get the data that is passed in the UI 
+    Route::get('/staffReportByUser', [StaffController::class, 'getReportByUser']); //This route should get the data that is passed in the UI 
+
+    Route::get('/recordsReport/{reportID}', [RecordsStatistics::class, 'getReport']); //This route should get the data that is passed in the UI 
+    Route::get('/recordsReportByUser', [RecordsStatistics::class, 'getReportByUser']); //This route should get the data that is passed in the UI 
+
+    Route::get('/HRReport/{reportID}', [HRStatistics::class, 'getReport']); //This route should get the data that is passed in the UI 
+    Route::get('/HRReportByUser', [HRStatistics::class, 'getReportByUser']); //This route should get the data that is passed in the UI H
+
+    Route::get('/financeReport/{reportID}', [FinanceStatistics::class, 'getReport']); //This route should get the data that is passed in the UI 
+    Route::get('/financeReportByUser', [FinanceStatistics::class, 'getReportByUser']); //This route should get the data that is passed in the UI H
+
+    //Update
+
+    Route::put('/facultyReport', [FacultyController::class, 'updateReport']); 
+
+    Route::put('/staffReport', [StaffController::class, 'updateReport']); 
+
+    Route::put('/recordsReport', [RecordsStatistics::class, 'updateReport']); 
+
+    Route::put('/HRReport', [HRStatistics::class, 'updateReport']); 
+
+    Route::put('/financeReport', [FinanceStatistics::class, 'updateReport']); 
+
+    //Delete
+
+    Route::delete('/facultyReport', [FacultyController::class, 'delReport']); 
+
+    Route::delete('/staffReport', [StaffController::class, 'delReport']); 
+
+    Route::delete('/recordsReport', [RecordsStatistics::class, 'delReport']); 
+
+    Route::delete('/HRReport', [HRStatistics::class, 'delReport']); 
+
+    Route::delete('/financeReport', [FinanceStatistics::class, 'delReport']); 
+
+    //Upload files
+
+
+    Route::post('/uploadPhoto', [FileUploadsController::class, 'uploadEventPhoto']); //This route should get the data that is passed in the UI 
+    Route::post('/uploadMeetings', [FileUploadsController::class, 'uploadMeetingMinutes']); //This route should get the data that is passed in the UI 
+    
+    /*Download files
+     The name of the contoller if FileUploadsController but I thought I could add in the download function in there one time 
+     as opposed to creating another controller for one function
+     You will pass the kind of file to download, by this I mean, you will pass if its a meeting or photo file, and file name(to download)*/
+
+    Route::get('/getFile/{fileType}/{fileName}', [FileUploadsController::class, 'downloadFile']);
+
+
+    /*Generate pdf file*/
+    Route::get('/generateStaffPdf/{reportID}', [StaffController::class, 'generateStaffPdf']);
+
+    Route::get('/generateFacultyPdf/{reportID}', [FacultyController::class, 'generateFacultyPdf']);
+
+    Route::get('/generateHRPdf/{reportID}', [HRStatistics::class, 'generateHRPdf']);
+
+    Route::get('/generateFinancePdf/{reportID}', [FinanceStatistics::class, 'generateFinancePdf']);
+
+    Route::get('/generateRecordsPdf/{reportID}', [RecordsStatistics::class, 'generateRecordsPdf']);
+
+    /*Return list of Reports*/
+    Route::get('/allReports/{reportTypes}', [ReportController::class, 'getReports']);
+
+
 });
+
+
+
