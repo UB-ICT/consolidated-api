095a9a55a6fe734cda2f33f9a84a1ceb9c916e6c
Luis <luis.herrera@ub.edu.bz>
2025-07-31 18:50:48 +0000

    remove remember token

--- Modules/Auth/database/migrations/2025_07_14_164402_alter-users-google-ssn.php
+++ Modules/Auth/database/migrations/2025_07_14_164402_alter-users-google-ssn.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::table('users', function (Blueprint $table) {
             $table->string('google_id')->nullable();
             $table->dateTime('email_verified_at')->default(now());
-            $table->rememberToken();
         });
     }
 
